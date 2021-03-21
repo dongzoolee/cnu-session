ba82c3228cf26a22523ad7f4443bd99d5c27ae4e
dongzoolee <nant0313@gmail.com>
2021-03-21 20:53:28 +0900

    Add 2Week Session Files

---
+++ 2week/back/sql.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2')
+const connection = mysql.createPool({
+    host: "cnudb.soga.ng",
+    user: "cnu",
+    password: "r912",
+    database: "cnu"
+})
+// 데이터베이스 시각화
+// http://viewcnudb.soga.ng
+
+
+// 쿼리문
+connection.query(`select * from user`, afterData)
+
+function afterData(error, result, field) {
+    if(error) {
+        console.log(error)
+        return
+    }
+
+    console.log(result)
+}
